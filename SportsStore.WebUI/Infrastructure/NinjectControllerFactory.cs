41320ffdc232742db00d2b4dd171d623f2d09bdb
gegMa3ai <zdv3223666@gmail.com>
2017-11-07 13:58:34 +0300

    Почти все работает

---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,30 @@
+﻿using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+        protected override IController GetControllerInstance(RequestContext requestContext,
+        Type controllerType)
+        {
+            return controllerType == null
+            ? null
+            : (IController)ninjectKernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
